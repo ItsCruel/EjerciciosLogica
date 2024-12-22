154606e9de5b2e672303e5637ef8ddf42d36a72b
CRUEL <oscuro34530@gmail.com>
2024-12-21 18:45:30 -0600

    primer ejercicio

---
+++ untitled/src/FizzBuzz.java
@@ -0,0 +1,13 @@
+
+/*
+ * Imprime los números del 1 al 100.
+ * Para los múltiplos de 3, imprime "Fizz".
+ * Para los múltiplos de 5, imprime "Buzz".
+ * Para los múltiplos de ambos, imprime "FizzBuzz".
+ */
+
+
+public class FizzBuzz {
+    for (int i )
+
+}
